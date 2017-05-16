535fc502a3925f476efcfe1820b3cf7a5ebb4b25
Patrick Berczelly <patrick.berczelly@grenoble-em.com>
2017-05-16 16:27:13 +0200

    exo ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+for i in (1..20)
+  Article.create(title: Faker::HarryPotter.character, content:Faker::ChuckNorris.fact)
+end
